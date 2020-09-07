919deebe04b549edf7b56fc5d0fde53bc714a772
Pavel Pyvovar <jalovanfres@gmail.com>
2020-09-07 17:21:47 +0300

    Bonus task completed

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # cvpr2020
-Repository for the Image Recognition course at Institute for Applied Systems Analysis (IASA)
+
+# Лаба №1: A toe into madness 
+Встановити OpenCV, зчитати зображення з вебки, відобразити в першому віконці та записати його на диск. Після цього зчитати щойно записане зображення з диску, конвертувати у відтінки сірого та намалювати на ньому довільних кольорів лінію та прямокутник (наприклад червону лінію та синій прямокутник) і відобразити у другому віконці. Ні це не психотест, для дебагу це ще й як знадобиться. Бонуси за виконання цих кроків для відеоряду і бонуси до бонусів якщо в результаті цих кроків замість звалища картинок матимемо відеофайл (наприклад .avi).
+
+# How to use this program?
+To take a picture of yourself call the `takeapic()` function. To show that picture use moudule `show_pic()`.
+
+For video_recording use `record_video()` module. To display that video call `display_video()`
+
+To run the program `main.py` run the following command in your CLI:
+    `python main.py`
+
+Good luck<3
+
+
--- main.py
+++ main.py
@@ -1,69 +1,114 @@
 import cv2
+import numpy as np
 
+# Part 1
 def takeapic(pic_name):
-    cap = cv2.VideoCapture(0)
+    """Function takes picture from webcam and converts it
+    into grayscale. Then saves image as jpg file.
 
+    Parameters:
+    pic_name: string
+        The name of the picture to be taken.
+
+    """
+    cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow(pic_name, gray)
-        pic = np.copy(gray)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow(pic_name, frame)
+        # Press key "q" to exit
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            pic = cv2.line(pic, (0, 0), (500, 500), (255, 0, 0), 20)
-            pic = cv2.rectangle(pic, (100, 100), (400, 400), (255, 0, 0), 5)
-            cv2.imwrite(f'{pic_name}.jpg', pic)
+            cv2.imwrite(f'{pic_name}.jpg', frame)
             break
     cleanup(cap)
 
 
 def show_pic(pic_name):
-    pic = cv2.imread(pic_name)
-    cv2.imshow(pic_name, pic)
+    """Function displays picture with colored line and rectangle
+    drawn on top of it.
+
+    Parameters:
+    pic_name: string
+        The name of the picture to be taken.
+    """
+    frame = cv2.imread(f'{pic_name}.jpg')
+    frame = cv2.line(frame, (0, 0), (500, 500), (255, 0, 0), 20)
+    frame = cv2.rectangle(frame, (100, 100), (400, 400), (0, 255, 0), 5)
+    cv2.imshow(pic_name, frame)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 
+# Part 2
 def record_video(video_name):
-    cap = cv2.VideoCapture(0)
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    vwriter = cv2.VideoWriter(f'{video_name}.avi', fourcc, 20, (640, 480))
+    """Function records grayscale video and saves it in .avi
+    file format.
 
+    Parameters:
+    video_name: string
+        The name of the video to be taken.
+    """
+    cap = cv2.VideoCapture(0)
+    # XVID works for some of the distributions of Linux.
+    # Please, note that for your OS this option may not work.
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    vwriter = cv2.VideoWriter(f'{video_name}.avi', fourcc, 20,
+                                (640, 480), isColor=False)
+    # The isColor option is required for saving video as grayscale.
     while cap.isOpened():
         ret, frame = cap.read()
         if ret:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            frame = cv2.line(frame, (0, 0), (500, 500), (255, 0, 0), 20)
-            frame = cv2.rectangle(frame, (100, 100), (400, 400), (255, 0, 0), 5)
+            vwriter.write(frame)
             cv2.imshow('frame', frame)
-
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+        else:
+            break
 
     cleanup(cap)
     vwriter.release()
 
 
 def display_video(video_name):
+    """Function displays video and draws a line and a rectangle
+    on top of it.
+
+    Parameters:
+    video_name: string
+        The name of the video to be taken.
+    """
+
     cap = cv2.VideoCapture(f'{video_name}.avi')
     while cap.isOpened():
         ret, frame = cap.read()
-        print(ret)
-        cv2.imshow(video_name, frame)
-
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        if ret:
+            frame = cv2.line(frame, (0, 0), (500, 500), (10, 255, 10), 20)
+            frame = cv2.rectangle(frame, (100, 100), (400, 400), (255, 0, 0), 5)
+            cv2.imshow(video_name, frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        else:
             break
-
     cleanup(cap)
 
 
 def cleanup(cap):
+    """Utility function for cleanup after work with webcam.
+
+    Parameters:
+        cap: cv2.VideoCapture
+        OpenCV object used to show and record videos.
+    """
     cap.release()
     cv2.destroyAllWindows()
 
 
 def main():
-    # record_video('kek')
-    display_video('kek')
+    # takeapic('lol')
+    # show_pic('lol')
+    # record_video('lol')
+    display_video('lol')
 
 
 if __name__ == "__main__":
